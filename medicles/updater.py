35cd08e59114b772e2d8d8fbfda8dc7d92b1fd11
Ramazan Kilimci <ramazankilimci@gmail.com>
2021-05-25 06:38:47 +0300

    Rocket science

--- medicles/updater.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-from apscheduler.schedulers.background import BackgroundScheduler
-from medicles import services
-
-def start():
-    scheduler = BackgroundScheduler()
-    scheduler.add_job(services.update_db, 'interval', minutes=1)
-    print("Updater starts")
-    scheduler.start()
